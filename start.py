3d4302a9b3a37632a443614630a39f104adc92e3
lalaio1 <122741162+lalaio1@users.noreply.github.com>
2024-10-26 23:22:37 -0300

    Add files via upload

--- start.py
+++ start.py
@@ -1,45 +1,19 @@
-import subprocess
 import os
-from func.instalar_bibliotecas_necessarias import instalar_bibliotecas_necessarias
-from func.verificar_e_definir_flag import verificar_e_definir_flag
-from func.setar_flag_true import setar_flag_true
 import sys
+import subprocess
 
-Cores = {
-    "vermelha": "\033[1;31m",
-    "verde": "\033[1;32m",
-    "amarela": "\033[1;33m",
-    "azul": "\033[1;34m",
-    "roxa": "\033[1;35m",
-    "ciano": "\033[1;36m",
-    "branca": "\033[1;37m",
-    "reset": "\033[0m",
-}
+def start_python_script(script_name):
+    if os.name == 'nt': 
+        subprocess.run(['python', script_name])
+    else: 
+        subprocess.run(['python3', script_name])
 
-def exibir_banner(titulo):
-    print(f"{Cores['azul']} {Cores['roxa']} {titulo}{Cores['reset']}")
+if __name__ == '__main__':
+    subprocess.run(['pip3', 'install', '-r', 'requirements.txt'])
+    script_to_run = r'./scripts/main.pyw'  
 
-def limpar_tela():
-    os.system("cls" if os.name == "nt" else "clear")
+    if not os.path.exists(script_to_run):
+        print(f"Error: The script {script_to_run} does not exist.")
+        sys.exit(1)
 
-def main():
-    limpar_tela()
-
-    flag_status = verificar_e_definir_flag()
-
-    if flag_status == "True":
-        if os.name == "nt":
-            exibir_banner("Dragondfa")
-        subprocess.run([sys.executable, "./conf/scripts/main.pyw"])
-    else:
-        exibir_banner("Iniciando a instalação das bibliotecas...")
-        instalar_bibliotecas_necessarias()
-        setar_flag_true()
-        print(f"{Cores['azul']}[{Cores['verde']}!{Cores['azul']}] Bibliotecas instaladas. Verificação concluída.{Cores['reset']}")
-        
-        if os.name == "nt":
-            exibir_banner("Dragondfa")
-        subprocess.run([sys.executable, "./conf/scripts/main.pyw"])
-
-if __name__ == "__main__":
-    main()
+    start_python_script(script_to_run)
